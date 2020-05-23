a3045cde04fa37769016d1d4db559d7f4f5ff8c0
Mario Russo <mario.russo@lastminute.com>
2020-05-23 17:29:43 +0200

    added some sample frontend in react

---
+++ src/main/resources/META-INF/resources/precache-manifest.24a574ed7957c738d8ab9a4ecf0dc6ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fcb4454787b75ae5f64d13f3c7e144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b191e7ded95297b0e1",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "a85a3711207cc282e8ce",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3b191e7ded95297b0e1",
+    "url": "/static/js/2.5e5af741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e5af741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85a3711207cc282e8ce",
+    "url": "/static/js/main.62b07862.chunk.js"
+  },
+  {
+    "revision": "a7706a487449e23a4855",
+    "url": "/static/js/runtime-main.325ca274.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
